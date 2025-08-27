f92f4c88e8a5378f8eee4db7e6208e3a8768afec
xkmld <154094054+xkmld@users.noreply.github.com>
2025-08-27 17:34:42 +0200

    add script.sh, README and .gitignore

---
+++ README.md
@@ -0,0 +1,13 @@
+# Date of Birth Web
+
+## Useful data
+```bash
+# For script
+https://atareao.es/tutorial/scripts-en-bash/arrays-en-bash/
+https://stackoverflow.com/questions/43158140/way-to-create-multiline-comments-in-bash
+https://unix.stackexchange.com/questions/156579/how-do-i-remove-spaces-from-shell-variables
+https://unix.stackexchange.com/questions/279760/how-can-we-use-multiple-variables-in-single-for-loop-in-shell-script
+https://stackoverflow.com/questions/638975/how-do-i-tell-if-a-file-does-not-exist-in-bash
+https://gist.github.com/JBlond/2fea43a3049b38287e5e9cefc87b2124
+https://askubuntu.com/questions/992336/how-to-extract-certain-data-from-a-line
+```
---
+++ script.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+months=(january february march april may june july august september october november december)
+TEMP_FILE=temp_month_file
+LINKS_FILE=links_file
+
+# colors
+RESET="\e[0m"
+RED="\e[0;31m"
+GREEN="\e[0;32m"
+YELLOW="\e[0;33m"
+BLUE="\e[0;34m"
+
+'
+The next step is for:
+- Generating the months with a number
+EXAMPLE:
+january1.html
+january2.html
+'
+if [ -f $TEMP_FILE ]; then
+	echo -e $YELLOW"skipping $TEMP_FILE, file exists" $RESET
+else
+	echo -e $GREEN"creating a file with the months and days" $RESET
+	for i in "${months[@]}"
+	do
+		for j in {1..31};
+		do
+			echo -n $i >> $TEMP_FILE
+			echo $j ".html" | tr -d ' ' >> $TEMP_FILE
+		done;
+	done;
+fi
+
+'
+The next step is for:
+	---> FROM:
+january1.html
+january2.html
+	---> TO:
+https://es.famousbirthdays.com/january1.html
+https://es.famousbirthdays.com/january2.html
+	--- EXPLANATION: We take the links of each person
+'
+if [ -f $LINKS_FILE ]; then
+	echo -e $YELLOW"skipping $LINKS_FILE, file exists" $RESET
+else
+	echo -e $GREEN"creating a link file..." $RESET
+	while read -r LINE
+	do
+		echo 'https://es.famousbirthdays.com/' "$LINE" | tr -d ' ' >> $LINKS_FILE
+	done < "$TEMP_FILE"
+fi
+
+echo -e $GREEN"removing $TEMP_FILE"
+rm -rf $TEMP_FILE
+
+ALL_LINKS_PEOPLE=links_href_grep
+
+'
+The next step is for:
+	---> FROM:
+https://es.famousbirthdays.com/january1.html
+	---> TO:
+				<a class="tile" href="https://es.famousbirthdays.com/people/morris-chestnut.html">
+
+	--- EXPLANATION: We take the links of each person
+'
+if [ -f $ALL_LINKS_PEOPLE ]; then
+	echo -e $YELLOW"skipping $ALL_LINKS_PEOPLE, file exists" $RESET
+else
+	echo -e $GREEN"creating all links people file..." $RESET
+	while read -r LINE
+	do
+		curl $LINE | grep "tile__item" -A 10 | grep "href=" >> $ALL_LINKS_PEOPLE
+	done < "$LINKS_FILE"
+fi
+
+# Now with this links we must clean and save only the links
+' 
+The next step is for:
+	---> FROM:
+				<a class="tile" href="https://es.famousbirthdays.com/people/morris-chestnut.html">
+	---> TO:
+https://es.famousbirthdays.com/people/morris-chestnut.html
+	--- EXPLANATION: Saving only the exact link and not the HTML
+'
+echo -e $GREEN"removing $LINKS_FILE"
+rm -rf $LINKS_FILE
+
+ALL_LINKS_PEOPLE_FILTERED=all_href_filtered
+
+if [ -f $ALL_LINKS_PEOPLE_FILTERED ]; then
+	echo -e $YELLOW"skipping $ALL_LINKS_PEOPLE_FILTERED, file exists" $RESET
+else
+	echo -e $GREEN"creating all links people FILTERED file..." $RESET
+	while read -r LINE
+	do
+		echo $LINE | tr -d '\t' | awk '{print $3}' | awk -F "\"" '{print $2}' >> $ALL_LINKS_PEOPLE_FILTERED
+		echo "copied: $LINE"
+		sleep 5
+	done < "$ALL_LINKS_PEOPLE"
+fi
+
