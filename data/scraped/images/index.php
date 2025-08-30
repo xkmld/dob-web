972c995d913b5dbd3fe70797b44e1f07731d4471
xkmld <154094054+xkmld@users.noreply.github.com>
2025-08-30 11:52:38 +0200

    add a command here

--- data/scraped/images/index.php
+++ data/scraped/images/index.php
@@ -1,5 +1,6 @@
 <?php
 // Author: https://stackoverflow.com/questions/10551135/display-files-in-the-folder-using-php
+// php -S 0.0.0.0:5500 -t . (to open server)
 $dir="."; // Directory where files are stored
 
 if ($dir_list = opendir($dir))
