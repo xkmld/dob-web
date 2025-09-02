1a605159f2e2fd985538f68a4c090faa277ae773
xkmld <154094054+xkmld@users.noreply.github.com>
2025-09-02 14:47:33 +0200

    move for the video and improve inseterer with try catch

--- utils/inserter/inserter.py
+++ utils/inserter/inserter.py
@@ -6,6 +6,7 @@ import json
 input_folder_name = "data/"
 scraped_data = "scraped/"
 total_folder_name = input_folder_name + scraped_data;
+print(total_folder_name)
 
 connection = psycopg2.connect(database="dob_web", user="dob_user", password="dob_pass", host="localhost", port=5432)
 
@@ -19,7 +20,10 @@ record = cursor.fetchall()
 print("Data from Database:- ", record)
 
 all_files = os.listdir(total_folder_name)
-all_files.remove("images")
+try:
+    all_files.remove("images")
+except ValueError:
+    print("images folder not in list")
 
 # to prevent problems...
 def return_if_exist(word):
@@ -32,7 +36,7 @@ def check_if_null(word):
     if (word == "null"):
         return None
     return word
- 
+
 def parse_birth_date(date):
     if (date == ""):
         return None
@@ -45,24 +49,27 @@ def process_name(name):
     return name.replace(' ', '').lower() + ".jpg"
 
 for file in all_files:
-    with open (total_folder_name + file) as f:
-        d = json.load(f)
+    with open (total_folder_name + file, encoding="utf8") as f:
+        try:
+            d = json.load(f)
         #if (d['img'] is None):
-        name = check_if_null(return_if_exist('name'))
-        job = check_if_null(return_if_exist('job'))
-        img = check_if_null(process_name(name))
-        birth_date = check_if_null(return_if_exist('birth_date'))
-        birth_date = parse_birth_date(birth_date)
-        if (birth_date == None):
-            continue
-        birth_place = check_if_null(return_if_exist('birth_place'))
-        about = check_if_null(return_if_exist('about'))
-        before_fame = check_if_null(return_if_exist('before_fame'))
-        curiosities = check_if_null(return_if_exist('curiosities'))
-        family_life = check_if_null(return_if_exist('family_life'))
-        association = check_if_null(return_if_exist('association'))
-        cursor.execute("INSERT INTO famous (name, job, img, birth_date, birth_place, about, before_fame, curiosities, family_life, association) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (name, job, img, birth_date,birth_place, about, before_fame, curiosities, family_life, association))
-            
+            name = check_if_null(return_if_exist('name'))
+            job = check_if_null(return_if_exist('job'))
+            img = check_if_null(process_name(name))
+            birth_date = check_if_null(return_if_exist('birth_date'))
+            birth_date = parse_birth_date(birth_date)
+            if (birth_date == None):
+                continue
+            birth_place = check_if_null(return_if_exist('birth_place'))
+            about = check_if_null(return_if_exist('about'))
+            before_fame = check_if_null(return_if_exist('before_fame'))
+            curiosities = check_if_null(return_if_exist('curiosities'))
+            family_life = check_if_null(return_if_exist('family_life'))
+            association = check_if_null(return_if_exist('association'))
+            cursor.execute("INSERT INTO famous (name, job, img, birth_date, birth_place, about, before_fame, curiosities, family_life, association) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (name, job, img, birth_date,birth_place, about, before_fame, curiosities, family_life, association))
+        except Exception as e: print(e)
+
+
 connection.commit();
 cursor.close()
 connection.close()
