88befd8791106590926e59ed11d14d3431eec0a3
xkmld <154094054+xkmld@users.noreply.github.com>
2025-09-06 13:47:30 +0200

    add navbar

--- web/front/src/components/Navbar.svelte
+++ web/front/src/components/Navbar.svelte
@@ -1,5 +1,9 @@
 <nav>
-	<a href="/">home</a>
-	<!--<a href="/about">about</a>-->
-	<a href="/famous">famous</a>
+<a href="/">home</a>
+<a href="/famous">famous</a>
+<!--<a href="/about">about</a>-->
+<form action="/action_page.php">
+<input placeholder="search" type="search" id="gsearch" name="gsearch">
+<input type="submit">
+</form>
 </nav>
